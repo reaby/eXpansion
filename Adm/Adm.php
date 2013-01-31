d1fd9dba18c299bec796b75e54b4af56c243c9cf
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2013-01-31 22:15:10 +0200

    Standard\Menubar integration

--- Adm/Adm.php
+++ Adm/Adm.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace ManiaLivePlugins\eXpansion\Adm;
 
 use \ManiaLivePlugins\eXpansion\Adm\Gui\Windows\ServerOptions;
 use ManiaLive\Gui\ActionHandler;
+
 class Adm extends \ManiaLive\PluginHandler\Plugin {
 
     function onInit() {
@@ -10,53 +12,25 @@ class Adm extends \ManiaLive\PluginHandler\Plugin {
     }
 
     function onReady() {
-    //    $methods = get_class_methods($this->connection);
-        ServerOptions::$actionOK = ActionHandler::getInstance()->createAction(array($this, "serverOptionsOk"));
-        ServerOptions::$actionCancel = ActionHandler::getInstance()->createAction(array($this, "serverOptionsCancel"));
-    
-          foreach ($this->storage->players as $player) {
-                $info = ServerOptions::Create($player->login);		
-                $info->setTitle('Server Options');
-		$info->centerOnScreen();
-                $info->setSize(160,100);
-                $info->show();                
-            }  
-        }
-    
-
-
-        
-    public function serverOptionsOk($login) {
-        print "ok";
-        
-        $server = $this->storage->server;
-
-        $serverOptions = Array(
-            "Name" => $this->serverName->getText(),
-            "Comment" => $this->serverComment->getText(),
-            "Password" => $this->serverPass->getText(),
-            "PasswordForSpectator" => $this->serverSpecPass->getText(),
-            "NextCallVoteTimeOut" => $server->currentCallvoteTimeOut,
-            "CallVoteRatio" => $server->callVoteRatio,
-            "RefereePassword" => $this->refereePass->getText(),
-            "IsP2PUpload" => $this->cbAllowp2pUp->getStatus(),
-            "IsP2PDownload" => $this->cbAllowp2pDown->getStatus(),
-            "AllowMapDownload" => $this->cbAllowMapDl->getStatus(),
-            "NextMaxPlayer" => $this->maxPlayers->getText(),
-            "NextMaxSpectator" => $this->maxSpec->getText(),
-            "RefereeMode" => $this->cbReferee->getStatus()
-        );
-
-
-        $this->connection->setServerOptions($serverOptions);
-
-        ServerOptions::Erase($login);
+        //    $methods = get_class_methods($this->connection);
+        if ($this->isPluginLoaded('Standard\Menubar'))
+            $this->buildMenu();
+      
     }
 
-    public function serverOptionsCancel($login) {
-        print "cancel";
-        ServerOptions::Erase($login);
+    public function buildMenu() {
+        $this->callPublicMethod('Standard\Menubar', 'initMenu', \ManiaLib\Gui\Elements\Icons128x128_1::Options);
+        $this->callPublicMethod('Standard\Menubar', 'addButton', 'Options', array($this, 'serverOptions'), true);        
     }
+    
+    public function serverOptions($login) {
+          $info = ServerOptions::Create($login);
+          $info->setTitle('Server Options');
+          $info->centerOnScreen();
+          $info->setSize(160,100);
+          $info->show();            
+    }
+
 }
 
 ?>
